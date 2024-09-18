53f091deaa05fcb7881bf11ac43e12bb3802d0e3
brunaflores-gete <80692164+brunaflores-gete@users.noreply.github.com>
2024-09-18 11:01:58 -0300

    ajustes

--- assets/js/geral.js
+++ assets/js/geral.js
@@ -32,3 +32,49 @@ $(window).scroll(function () {
   }
 
 }); // scroll END
+
+
+// MODO ESCURO
+$(window).scroll(function () {
+
+  var topPos = $(this).scrollTop();
+
+  // if user scrolls down..
+  if (topPos > 0) {
+    $('body').removeClass("top60");
+    $('body').addClass("top00");
+    $('.bt-hamburguer').css("top", "20px");
+    $('.bt-fecharmenu').css("top", "20px");
+  } else {
+    $('body').removeClass("top00");
+    $('body').addClass("top60");
+    $('.bt-hamburguer').css("top", "150px");      
+    $('.bt-fecharmenu').css("top", "150px");      
+  }
+
+});
+var isDarkMode = true; // Controla o modo atual (escuro/claro)
+
+function toggleBackgroundImage() {
+    var button = document.getElementById('toggleButton');
+    isDarkMode = !isDarkMode; // Alternar entre escuro e claro
+
+    if (isDarkMode) {
+        button.style.backgroundImage = "url('../assets/images/modo_escuro.svg')";
+    } else {
+        button.style.backgroundImage = "url('../assets/images/modo_claro.svg')";
+    }
+}
+
+$(".btn-tema_pagina").click(function () {
+  console.log($(".btn-tema_pagina"))
+  if ($("body").hasClass('bg-claro')) {
+      $("body").removeClass('bg-claro')
+      $("body").addClass("bg-escuro");
+  } else if ($("body").hasClass('bg-escuro')) {
+      $("body").removeClass('bg-escuro')
+      $("body").addClass("bg-claro")
+  }
+});
+
+// MODO ESCURO
